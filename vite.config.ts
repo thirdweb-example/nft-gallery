ad3f4be5bd338beb3977c1fb1654e9f5f5baa77d
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2023-05-15 17:54:31 +0530

    fix: config (#6)

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      global: true,
+    }),
+  ],
   define: {
     "process.env": {},
-    global: {},
   },
   resolve: {
     alias: {
