dfc4b3917a69243c76de5c4381a8e5960f936295
Jarrod Watts <35651410+jarrodwatts@users.noreply.github.com>
2023-05-18 14:41:34 +1000

    Update parameters.ts

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -1,3 +1,13 @@
+/** Change these values to configure the application for your own use. **/
+
+// Your smart contract address (available on the thirdweb dashboard)
+// For existing collections: import your existing contracts on the dashboard: https://thirdweb.com/dashboard
 export const contractAddress = "0xed5af388653567af2f388e6224dc7c4b3241c544";
-export const blockExplorer = "https://etherscan.io";
+
+// The name of the chain your contract is deployed to.
+// Provide a string for standard chains, or import one from @thirdweb-dev/chains package.
+// Learn more: https://portal.thirdweb.com/react/react.thirdwebprovider#activechain-recommended
 export const chain = "ethereum";
+
+// The block explorer you want to use (Opens when user clicks on history of events. i.e. transfers)
+export const blockExplorer = "https://etherscan.io";
